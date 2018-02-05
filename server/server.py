e3a0d37ea7b3dc50f34a65483ec9eb3e497041f4
Pentium <lawd3@uni.coventry.ac.uk>
2018-02-05 16:25:11 +0000

    Created attening method
    method adds users to the attendance DB using their student_id and the event_uuid of the room

--- server/server.py
+++ server/server.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import attending
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/attend")
+def attend():
+        student_id = request.args.get('user')
+        event_uuid = request.args.get('event')
+        return attending.attend(student_id,event_uuid)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
