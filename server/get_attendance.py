ef91ed0d0cac6db9490548274c360b6133f3f511
Pentium <lawd3@uni.coventry.ac.uk>
2018-02-05 16:48:11 +0000

    Creating the get attendance method
    method gets attendance from a students ID and returns it as a dictionary

---
+++ server/get_attendance.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def attendance():
+    conn = sqlite3.connect('insertPathHere')
+    c = conn.cursor()
+"""
+    jsonify the dictionary 
+"""
--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import attending
+import get_attendance
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,9 +9,14 @@ def hello_world():
 
 @app.route("/attend")
 def attend():
-        student_id = request.args.get('user')
-        event_uuid = request.args.get('event')
-        return attending.attend(student_id,event_uuid)
+    student_id = request.args.get('user')
+    event_uuid = request.args.get('event')
+    return attending.attend(student_id,event_uuid)
+
+@app.route("/placeholder")
+def student_attendance():
+    student_id = request.args.get('user')
+    return get_attendance.attendance():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
