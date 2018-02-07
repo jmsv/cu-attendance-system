97f73aa9e169cce9144844ad8c30b73faa28dd9d
Pentium <lawd3@uni.coventry.ac.uk>
2018-02-07 23:17:59 +0000

    Added time to attend method
    now inserts current time when loging attendance

--- server/attending.py
+++ server/attending.py
@@ -1,11 +1,14 @@
 import sqlite3
+import datetime
 
 db_path = 'database.db'
 
 
-def attend(student_id,arrival,event_uuid):
+def attend(student_id,event_uuid):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
+    arrival = datetime.datetime.now()
+    arrival = arrival.strftime("%Y-%m-%d %H:%M:%S")
     c.execute("INSERT INTO Attendance VALUES(?,?,?);",(student_id,arrival,event_uuid))
     conn.commit()
     conn.close()
