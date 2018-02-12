b6660b100ebf67b165804763cacccf0cebd0982c
James Vickery <dev@jamesvickery.net>
2018-02-12 16:32:13 +0000

    Updated database tables

--- server/dbscripts/create-tables.sql
+++ server/dbscripts/create-tables.sql
@@ -1,7 +1,34 @@
-CREATE TABLE Attendance (
-  sid INTEGER NOT NULL,
-  arrival DATETIME, /* Input in format: '2007-01-01 10:00:00' */
-  event_id VARCHAR(16) NOT NULL
+CREATE TABLE Student(
+  sid INT NOT NULL PRIMARY KEY,
+  name VARCHAR(64),
+  username VARCHAR(16)
+);
+
+CREATE TABLE Lecturer(
+  lecturer_id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(64),
+  password_hash VARCHAR(64)
+);
+
+CREATE TABLE Events(
+  event_id VARCHAR(16) NOT NULL PRIMARY KEY,
+  room VARCHAR(8),
+  datetime_start DATETIME,
+  datetime_end DATETIME,
+  lecturer_id INT,
+
+  FOREIGN KEY(lecturer_id) REFERENCES Lecturer(lecturer_id)
+);
+
+CREATE TABLE Attendance(
+  sid INT NOT NULL,
+  arrival DATETIME NOT NULL,
+  event_id VARCHAR(16) NOT NULL,
+
+  PRIMARY KEY (sid, event_id),
+
+  FOREIGN KEY(event_id) REFERENCES Events(event_id),
+  FOREIGN KEY(sid) REFERENCES Student(sid)
 );
 
 
