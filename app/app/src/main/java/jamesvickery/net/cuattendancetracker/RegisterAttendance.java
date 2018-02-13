2b81cf99cf5347a4f957beb5bfd6c8f805c5d220
James Vickery <dev@jamesvickery.net>
2018-02-13 01:56:04 +0000

    Working QR code sign in

--- app/app/src/main/java/jamesvickery/net/cuattendancetracker/RegisterAttendance.java
+++ app/app/src/main/java/jamesvickery/net/cuattendancetracker/RegisterAttendance.java
@@ -2,17 +2,25 @@ package jamesvickery.net.cuattendancetracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class RegisterAttendance extends AppCompatActivity {
+
+    String eventId = "";
+    TextView textViewEventId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +34,25 @@ public class RegisterAttendance extends AppCompatActivity {
 
         setContentView(R.layout.activity_register_attendance);
 
+        Button buttonBackHome = findViewById(R.id.buttonBackHome);
+        buttonBackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setTitle("this is the code thing");
-//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                finish();
-//            }
-//        });
-//        builder.setMessage(eventId);
-//        AlertDialog alert = builder.create();
-//        alert.show();
-
-        TextView textViewEventId = findViewById(R.id.textViewEventId);
+        textViewEventId = findViewById(R.id.textViewEventId);
         textViewEventId.setText("signing into: " + eventId);
+
+        registerAttendancePost(eventId);
     }
 
-    boolean registerAttendancePost(String event_id) {
+    void registerAttendancePost(String event_id) {
         OkHttpClient client = new OkHttpClient();
 
         RequestBody formBody = new FormBody.Builder()
-                .add("user", "7000000")
+                .add("user", "666")
                 .add("event", event_id)
                 .build();
 
@@ -56,13 +61,36 @@ public class RegisterAttendance extends AppCompatActivity {
                 .post(formBody)
                 .build();
 
-        try {
-            Response response = client.newCall(request).execute();
+        // TODO: Better error checking in the http request below
 
-            if (response.code() == 200) return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+//                    Headers responseHeaders = response.headers();
+//                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+//                        // Toast doesn't work here
+//                        // responseHeaders.name(i) + ": " + responseHeaders.value(i)
+//                    }
+
+//                    // responseBody.string()
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewEventId.setText(textViewEventId.getText() + "... success!");
+                        }
+                    });
+                }
+            }
+        });
     }
 }
