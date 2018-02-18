4782a0168f8402b350946c56d691a34355483505
James Vickery <dev@jamesvickery.net>
2018-02-18 03:59:15 +0000

    Lots of UI work on lecturer webapp

---
+++ server/static/lecturer-webapp/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('cuasApp', ["ngRoute"]);
+
+app.controller('DashboardController', function ($scope) {
+  $scope.displayName = "Dennis";
+  $scope.greeting = "";
+
+  var today = new Date()
+  var curHr = today.getHours()
+
+  if (curHr < 12) {
+    $scope.greeting = 'Good morning';
+  } else if (curHr < 18) {
+    $scope.greeting = 'Good afternoon';
+  } else {
+    $scope.greeting = 'Good evening';
+  }
+
+  $scope.greeting += ', ' + $scope.displayName + '!';
+});
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "static/lecturer-webapp/dashboard.html"
+    })
+    .when("/test", {
+      templateUrl: "static/lecturer-webapp/test.html"
+    })
+    .when("/start", {
+      templateUrl: "static/lecturer-webapp/start-lesson.html"
+    })
+    .when("/lessons", {
+      templateUrl: "static/lecturer-webapp/lessons.html"
+    });
+});
