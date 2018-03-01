3a03ad819f129b95a0ad62bc050f0f8908515e0e
James Vickery <dev@jamesvickery.net>
2018-03-01 21:22:48 +0000

    Made app prettier

--- app/app/src/main/java/jamesvickery/net/cuattendancetracker/MainActivity.java
+++ app/app/src/main/java/jamesvickery/net/cuattendancetracker/MainActivity.java
@@ -28,15 +28,6 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(signInIntent);
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
